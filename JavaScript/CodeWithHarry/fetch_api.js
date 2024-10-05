3f157a1a7a1fb508d85f137c8a79358ce35ff277
Saksham Sharma <85439193+SxmCodes@users.noreply.github.com>
2024-10-05 23:41:12 +0530

    Update fetch_api.js

--- JavaScript/CodeWithHarry/fetch_api.js
+++ JavaScript/CodeWithHarry/fetch_api.js
@@ -8,7 +8,7 @@ let content = document.getElementById("content");
 // This is get request with fetch api.
 function getData() {
   // This will ask for promise. We can pass another url like api.github.com/users
-  url = "saksham.txt";
+  url = "User.txt";
   fetch(url)
     .then((response) => {
       // We can write json here if we have to pass the api users data as json format, it will parse the data.
@@ -22,7 +22,7 @@ function getData() {
 // This is post request via fetch api.
 function postData() {
   url = "dummy.restapi.example.com/api/v1/create";
-  data = '{name:"Saksham", salary:"120000000", age="16"}'
+  data = '{name:"User", salary:"120000000", age="16"}'
 
   params = {
     method: 'post',
